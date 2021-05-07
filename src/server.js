d7d15fe9f10539f61ba547664b5072c43c8efe19
herbertbryan <herbert.bryan@gmail.com>
2021-05-07 01:02:41 -0300

    Rota para listagem de arquivos na pasta upload.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path')
 require('dotenv').config();
 
 
@@ -19,7 +21,9 @@ mongoose.connect(URL, {useNewUrlParser: true,useUnifiedTopology: true,})
 // req.params = acessar route params (para edição, delete)
 // req.body = acessar corpo da requisição (ciração/edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..','uploads')));
 app.use(routes);
 
 
